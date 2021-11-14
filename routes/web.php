48e5d19c8e2edbbafbc809778594ced729765892
Md. Joynal Alam <59138477+joynalam67598@users.noreply.github.com>
2021-11-14 23:21:13 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
     use Illuminate\Support\Facades\Auth;
     use Illuminate\Support\Facades\Route;
 
-
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 //------------------- FRONTEND --------------------------
 
     //-------------------home----------------------
